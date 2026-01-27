fc403944a047125865df6f34fc440cb7e37090e9
Feniks7002 <07piotrr07@gmail.com>
2026-01-27 18:50:09 +0100

    Pierwsze działające oprogramowanie do generowania sinusoidalnego rysunku podanej grafiki

---
+++ src/config.py
@@ -0,0 +1,13 @@
+WORK_WIDTH = 1000
+WORK_HEIGHT = 1000
+
+IMG_PATH_E = 'C:/Users/PC/Desktop/Projekty informatyka/Projekt DIY plotter/DIY-plotter/res/elon.png'
+IMG_PATH_L = 'C:/Users/PC/Desktop/Projekty informatyka/Projekt DIY plotter/DIY-plotter/res/lena.png'
+
+LINE_SPACING = 5
+Y_OFFSET = 5
+
+SINUS_MAX_AMPLITUDE = 5
+SINUS_FREQUENCY = 0.5
+
+CONTRAST_FACTOR = 2.0
---
+++ src/sinusoid_engine.py
@@ -0,0 +1,56 @@
+import math
+import tkinter as tk 
+import numpy as np
+import config
+from PIL import Image, ImageEnhance, ImageDraw
+
+def image_handler(path, factor, max_w, max_h):
+    def contrast_enchance(img):
+        img_contrast_enchance = ImageEnhance.Contrast(img_gray)
+        return img_contrast_enchance.enhance(factor)
+    
+    image = Image.open(path)
+    image.thumbnail((max_w, max_h))
+
+    img_gray = image.convert('L')
+
+    img_gray_enchanced = contrast_enchance(img_gray)    
+    
+    img_array = np.array(img_gray_enchanced)
+    img_normalized = img_array / 255.0
+    return img_normalized
+
+def amplitude_map(data, max_amp):
+    return (1.0 - data) * max_amp
+
+def trajectory_math(amp_map, y_base, freq, line_spacing):
+    trajectory = []
+    for y_index in range(0, amp_map.shape[0], line_spacing):
+        amplitudes = amp_map[y_index, :]
+        line_points = []
+        for x, amp in enumerate(amplitudes):
+            y = y_base + math.sin(x * freq) * amp
+            line_points.append((x, y))
+        trajectory.append(line_points)
+        y_base += line_spacing
+    return trajectory
+
+def simulation(trajectory, w, h):
+    root = tk.Tk()
+    canvas = tk.Canvas(root, width=w, height=h, bg='white')
+    canvas.pack()
+
+    for line in trajectory:
+        for i in range(len(line)-1):
+            x1, y1 = line[i]
+            x2, y2 = line[i + 1]
+            canvas.create_line(x1, y1, x2, y2, fill='black')
+    root.mainloop()
+
+
+
+converted_image_data = image_handler(config.IMG_PATH_L, config.CONTRAST_FACTOR, config.WORK_WIDTH, config.WORK_HEIGHT)
+amplitude = amplitude_map(converted_image_data, config.SINUS_MAX_AMPLITUDE)
+trajectory = trajectory_math(amplitude, config.Y_OFFSET, config.SINUS_FREQUENCY, config.LINE_SPACING)
+
+simulation(trajectory, config.WORK_WIDTH, config.WORK_HEIGHT)
