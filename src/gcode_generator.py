0e8bce91c9ed90030cb91a9b0cfb6907f15b4a43
Feniks7002 <07piotrr07@gmail.com>
2026-09-20 19:27:30 +0200

    Creating a custom gcode generator

---
+++ src/gcode_generator.py
@@ -0,0 +1,50 @@
+
+class GCodeGenerator:
+    def __init__(self, trajectory_mm, z_positions, result_path, feedrate_travel=3000, feedrate_draw=1500):
+        self.trajectory_map = trajectory_mm
+        self.f_travel = feedrate_travel
+        self.f_draw = feedrate_draw
+        self.z_up, self.z_down = z_positions
+        self.path = result_path
+
+    def generate_code(self):
+        gcode = [
+            "; --- PREAMBLE ---",
+            "G21 ; Millimeters",
+            "G90 ; Absolute coordinates",
+            f"G00 Z{self.z_up:.2f} F{self.f_travel}"
+        ]
+
+        if not self.trajectory_map or not self.trajectory_map[0]:
+            return gcode
+
+        # Dojazd do pierwszego punktu
+        first_x, first_y = self.trajectory_map[0][0]
+        gcode.append(f"G00 X{first_x:.2f} Y{first_y:.2f}")
+        gcode.append(f"G01 Z{self.z_down:.2f} F{self.f_draw}")
+        gcode.append(f"G04 P0.2")
+
+        # Rysowanie całej trajektorii
+        gcode.append(f"; --- TRAJECTORY ---")
+        for line in self.trajectory_map:
+            for x, y in line:
+                gcode.append(f"G1 X{x:.2f} Y{y:.2f} F{self.f_draw}")
+
+        # Zakończenie pracy
+        gcode.extend([
+            "; --- POSTAMBLE ---",
+            f"G00 Z{self.z_up:.2f} F{self.f_travel}",
+            "G00 X0.00 Y0.00",
+            "M02 ; End of program"
+        ])
+
+        return gcode
+
+    def save_file(self, path, gcode_list):
+        with open(path, "w", encoding="utf-8") as file:
+            for element in gcode_list:
+                file.write(element + "\n")
+
+    def run(self):
+        generated_code = self.generate_code()
+        save = self.save_file(self.path, generated_code)
--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from image_handler import ImageHandler
 from sinusoid_engine import SinusEngine, TrajectoryNormalizer, StepsGenerator
 from simulation import Simulation
+from gcode_generator import GCodeGenerator
 import config
 
 def simulation(trajectory):
@@ -9,22 +10,18 @@ def simulation(trajectory):
 
 def main():
     image_instance = ImageHandler(config.IMG_PATH_L, (config.RENDER_WIDTH, config.RENDER_HEIGHT), config.CONTRAST_FACTOR)
-    
     image_processed = image_instance.image_processing()
 
     engine_instance = SinusEngine(image_processed, (config.WORK_WIDTH, config.WORK_HEIGHT), (config.RENDER_WIDTH, config.RENDER_HEIGHT), (config.SINUS_MAX_AMPLITUDE, config.SINUS_FREQUENCY, config.LINE_SPACING), (config.X_OFFSET, config.Y_OFFSET))
-
     raw_trajectory = engine_instance.run()
     
     simulation(raw_trajectory)
 
     trajectory_normalizer_instance = TrajectoryNormalizer(raw_trajectory, (config.WORK_WIDTH, config.WORK_HEIGHT), config.LINE_SPACING)
-    
     full_trajectory = trajectory_normalizer_instance.run()
 
-    step_generator_instance = StepsGenerator(full_trajectory, (config.STEPS_PER_MM))
-
-    step_generator_instance.run()
+    gcode_instance = GCodeGenerator(full_trajectory, (config.Z_UP, config.Z_DOWN), config.GCODE_PATH)
+    gcode_generate = gcode_instance.run()
 
 if __name__ == "__main__":
     main()
