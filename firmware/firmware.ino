f23306d8e75ebcf47a9d700e7b390e6faed0601b
Feniks7002 <07piotrr07@gmail.com>
2026-09-25 18:32:49 +0200

    Motion controller integrated  into main firmware loop

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,5 +1,6 @@
 #include "config.h"
 #include "parser.h"
+#include "motion.h"
 
 // Bufor danych
 const int buffer_size = 64;
@@ -8,7 +9,11 @@ static uint8_t buffer_idx = 0;
 
 void setup() {
     Serial.begin(BAUD_RATE);
+
+    motion_init();
+
     while (!Serial) { ; }
+
     Serial.println(F("DIY Pen Plotter Ready"));
 }
 
@@ -25,7 +30,7 @@ void loop() {
             if (buffer_idx > 0){
                 GCodeStructure cmd;
                 if (parse_gcode_line(line_buffer, cmd)) {
-                    // Miejsce na wywołanie modułu motion w Kroku 1.3
+                    motion_execute(cmd);
                     Serial.println(F("ok"));
                 } else {
                     Serial.println(F("error: invalid command"));
